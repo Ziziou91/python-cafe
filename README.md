fd9306efcadce6ce959367c5cb879d404842a268
Josh Gray <joshgray91@gmail.com>
2024-01-08 22:51:22 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Python cafe app
+
+A simple application to practice working with dictonaties and lists.
+
+The app simulates a cafe and the user can interact with it as either:
+- The owner: view current stock level and total value, as well as production cost and value of each dish 
+- A patron: order items from the menu 
---
+++ cafe.py
@@ -0,0 +1,8 @@
+"""A simple application to practice working with dictonaties and lists.
+
+The app simulates a cafe and the user can interact with it as either:
+- The owner: view current stock level and total value, as well as production cost and value of each dish 
+- A patron: order items from the menu (unless they've run out), tell the staff your finished and get your bill
+"""
+
+menu = ["Pasta"]
